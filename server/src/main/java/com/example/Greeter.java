b0e2322ced00fc95aa66cb1dac55a46a9276ed2d
amruthbomma <bomma.cloud6@gmail.com>
2017-03-07 11:37:41 -0500

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,12 @@ public class Greeter {
 
   }
 
-  public String greet(final String someone) {
+  /**
+  *@param some one the name of the person
+  *@return greeting string 
+  **/
+  
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
