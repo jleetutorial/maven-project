32ec7ee5ab7971f8a708dd61fdb909d25d089165
ahmed1102003 <ahmed1102003@yahoo.com>
2017-08-08 13:58:08 -0700

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,9 @@ public class Greeter {
   public Greeter() {
 
   } 
-/*
-  *@return greeting string
-  */
+ /**
+   * This is a constructor for .
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
