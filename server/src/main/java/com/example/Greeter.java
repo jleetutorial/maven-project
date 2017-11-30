b28d2f784ed60e71861b85411b5e9ee0c7deea5e
Nikhil Pillai <33648644+pillainikhil@users.noreply.github.com>
2017-11-30 12:20:29 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,13 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ 
+  /**
+  * @param this is someone name
+  * @result this is result
+  */
+  
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
