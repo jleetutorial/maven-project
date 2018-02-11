2ec01d362d356ecf21f13d96fe4d32b8c7e00dba
amiteshCoder <amiteshshukla999@gmail.com>
2018-02-12 00:58:05 +0530

    for pipeline example

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @prama someone the name
+  * @return greeting string
+  */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
