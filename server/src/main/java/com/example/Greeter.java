188a93f1fda8d4c2bc3ac3d21634ba009594f364
viruszs <37419529+viruszs@users.noreply.github.com>
2018-03-15 17:49:56 -0500

    Add files via upload

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,8 @@ public class Greeter {
 
   }
 
-/**
-* @param someone the name of a person
-* @return greeting string 
-*/
-  public final String greet(final String someone) {
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
