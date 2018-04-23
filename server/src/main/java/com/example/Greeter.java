8900d1269349689ee3dd33f6cc879862e039bab1
ashisaho <ashish.sahook@gmail.com>
2018-04-23 20:27:59 +0530

    fixed'

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
   
   /**
-  * @param this is not
-  * @return this not known
+  * @param someone the name of the person
+  * @return greeting string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -2,3 +2,4 @@
 * This is package name
 */
 package com.example;
+
