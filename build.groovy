4a07ab76eb67c8b1847a1dabfdfae59bbd7be9ab
prdur1 <32431793+prdur1@users.noreply.github.com>
2018-08-02 17:27:54 -0500

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -2,6 +2,14 @@ node {
   git url: 'https://github.com/prdur1/maven-project.git'
   def mvnHome = tool 'localMaven'
   env.PATH = "${mvnHome}/bin:${env.PATH}"
+  environment {
+    FOO = "BAR"
+    BUILD_NUM_ENV = currentBuild.getNumber()
+    ANOTHER_ENV = "${currentBuild.getNumber()}"
+    INHERITED_ENV = "\${BUILD_NUM_ENV} is inherited"
+    ACME_FUNC = readMavenPom().getArtifactId()
+  }
+  
   stage('File Loading')
   {
      echo 'I am from Example'
@@ -33,6 +41,24 @@ node {
           }
         }
      } 
+  stage('Enviroment')
+  {
+     sh 'echo "FOO is $FOO"'
+        // returns 'FOO is BAR'
+
+        sh 'echo "BUILD_NUM_ENV is $BUILD_NUM_ENV"'
+        // returns 'BUILD_NUM_ENV is 4' depending on the build number
+
+        sh 'echo "ANOTHER_ENV is $ANOTHER_ENV"'
+        // returns 'ANOTHER_ENV is 4' like the previous depending on the build number
+
+        sh 'echo "INHERITED_ENV is $INHERITED_ENV"'
+        // returns 'INHERITED_ENV is ${BUILD_NUM_ENV} is inherited'
+        // The \ escapes the $ so the variable is not expanded but becomes a literal
+
+        sh 'echo "ACME_FUNC is $ACME_FUNC"'
+        // returns 'ACME_FUNC is spring-petclinic' or the name of the artifact in the pom.xml
+  }
   
  /*stage('buid')
   {
