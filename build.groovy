518ff161963d0a3e1a48e6a82e199b733b7dd6d3
prdur1 <32431793+prdur1@users.noreply.github.com>
2018-08-02 14:20:37 -0500

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -4,12 +4,18 @@ node {
   env.PATH = "${mvnHome}/bin:${env.PATH}"
   stage('Example')
   {
-     echo 'I Example'
+     echo 'I am from Example'
     // Load the file 'externalMethod.groovy' from the current directory, into a variable called "externalMethod".
    def externalMethod = load("externalMethod.groovy")
 
     // Call the method we defined in externalMethod.
-   externalMethod.lookAtThis("Steve")
+   externalMethod.lookAtThis("Pradeep")
+    // Now load 'externalCall.groovy'.
+    def externalCall = load("externalCall.groovy")
+
+    // We can just run it with "externalCall(...)" since it has a call method.
+    externalCall("Steve")
+    
    }
   stage('buid')
   {
