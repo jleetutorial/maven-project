5febe27e1e6808ce0d08e2517a695701c1bd35c8
prdur1 <32431793+prdur1@users.noreply.github.com>
2018-08-02 00:16:01 -0500

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -2,5 +2,13 @@ node {
   git url: 'https://github.com/prdur1/maven-project.git'
   def mvnHome = tool 'localMaven'
   env.PATH = "${mvnHome}/bin:${env.PATH}"
+  stage('Example')
+  {
+     echo 'I Example'
+  }
+  stage('buid')
+  {
+    echo 'Build'
   sh 'mvn clean package'
+  }
 }
