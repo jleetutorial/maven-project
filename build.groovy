f93eb79cce37b6e220f29b61daa8672301407b3f
prdur1 <32431793+prdur1@users.noreply.github.com>
2018-08-02 14:08:49 -0500

    externalMethod.groovy

--- build.groovy
+++ build.groovy
@@ -6,7 +6,7 @@ node {
   {
      echo 'I Example'
     // Load the file 'externalMethod.groovy' from the current directory, into a variable called "externalMethod".
-   // def externalMethod = load("externalMethod.groovy")
+   def externalMethod = load("externalMethod.groovy")
 
     // Call the method we defined in externalMethod.
   //  externalMethod.lookAtThis("Steve")
