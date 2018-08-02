7449d8c2b6bf0486af3199940ea2b9d0e99242c7
prdur1 <32431793+prdur1@users.noreply.github.com>
2018-08-02 10:13:39 -0500

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -5,11 +5,11 @@ node {
   stage('Example')
   {
      echo 'I Example'
-     // Now load 'externalCall.groovy'.
-   // def externalCall = load("externalCall.groovy")
+    // Load the file 'externalMethod.groovy' from the current directory, into a variable called "externalMethod".
+    def externalMethod = load("externalMethod.groovy")
 
-    // We can just run it with "externalCall(...)" since it has a call method.
-    //externalCall("Steve")
+    // Call the method we defined in externalMethod.
+    externalMethod.lookAtThis("Steve")
    }
   stage('buid')
   {
