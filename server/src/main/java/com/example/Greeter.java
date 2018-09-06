4a7419bf4b4ea0963d649b0316baa50fa5a8734d
Himanshu Shukla <33247060+himanshushukla08@users.noreply.github.com>
2018-09-06 11:13:22 +0530

    added javadoc comment + final in method

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
+  
+  /**
+  *@param someone the name of the person
+  *@return greeting string
 
-  //TODO: Add javadoc comment
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
