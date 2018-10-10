91174845c680ddfe630687f8801f08720a75baad
Rajnish Singh4 <rajnish367@gmail.com>
2018-10-10 23:30:54 +0530

    added

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
