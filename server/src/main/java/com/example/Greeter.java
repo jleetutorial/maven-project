247fa7036d5b012a53f1b8c0e0c3fd4141233333
DD <DD@localhost.localdomain>
2018-10-11 01:54:05 -0400

    @return tag added

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
   /**
   * @param someone the name of a person
-  * return greeting string
+  * @return greeting string
   */
 
   public final String greet(final String someone) {
