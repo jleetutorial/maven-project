d546ab1eacdf6783082e1ddb9d2d826514c2913e
ArvindAKA <40854826+ArvindAKA@users.noreply.github.com>
2018-11-26 12:35:15 +0100

    fixed warnings and code comments

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
