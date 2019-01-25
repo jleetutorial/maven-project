961209a263b2c29f52c65322c17c31efd2739775
EC2 Default User <ec2-user@ip-172-31-17-201.eu-west-2.compute.internal>
2019-01-25 13:18:04 +0000

    checkstyle warnings fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/** @param the name of someone
+ *  @return greeting string
+ */
+
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
