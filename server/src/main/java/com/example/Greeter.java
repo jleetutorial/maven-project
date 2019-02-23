53da5e879f8bea6b6c75a094f315961b0fbaa2e4
Chirodip Pal <chirodip.pal@cathaypacific.com>
2019-02-23 04:36:13 +0000

    Added logging

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,22 +1,23 @@
 package com.example;
+import org.apache.log4j.Logger;
 
 /**
  * This is a class.
  */
 public class Greeter {
-
   /**
    * This is a constructor.
    */
+  static Logger logger = Logger.getLogger(Greeter.class);
   public Greeter() {
-
   }
-
   /**
    * @param someone the name of a person
    * @return greeting string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
+    logger.debug("This is debug message");
+    logger.info("This is info message");
   }
 }
