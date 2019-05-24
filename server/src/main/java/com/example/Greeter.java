9f3559650ea270057098f02f9b1646048c999f34
VM1 <robin@localhost.localdomain>
2019-05-24 15:19:43 +0530

    error corrected

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,6 @@ public class Greeter {
    * @param someone the name of a person
    * @return greeting string
    */
-	
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -2,4 +2,5 @@
  * this is a package name.
  */
 
-Package com.example;
+package com.example;
+	
