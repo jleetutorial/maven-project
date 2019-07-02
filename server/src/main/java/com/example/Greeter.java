409976a4478e9ff3778b0d7c34092d236c9fd269
arunstack <arunstacklogy@gmail.com>
2019-07-02 19:29:20 +0000

    bug fixed

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+	/**
+	* @someone  its a name of someone
+	* @return	Greetings
+	*/
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-Info.java
@@ -0,0 +1,4 @@
+/**
+*This is a package name.
+*/
+package com.example;
