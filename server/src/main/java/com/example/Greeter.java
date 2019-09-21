a9404791a3ec31450381b3218f28e6efd3f5a1ae
Prasad <pktanksali@gmail.com>
2019-09-21 14:41:33 +0000

    Updated files

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,22 +1,17 @@
 package com.example;
+
 /**
  * This is a class.
  */
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 public class Greeter {
+
   /**
    * This is a constructor.
    */
-  static Logger logger = LogManager.getLogger(Greeter.class);
   public Greeter() {
-    logger.debug("This is debug message");
-    logger.info("This is info message");
-    logger.warn("This is warn message");
-    logger.fatal("This is fatal message");
-    logger.error("This is error message");
-    System.out.println("Logic executed successfully....");
+
   }
+
   /**
    * @param someone the name of a person
    * @return greeting string
--- webapp/src/main/webapp/index.jsp
+++ webapp/src/main/webapp/index.jsp
@@ -1,1 +1,2 @@
 Hello, World!
+v1.1
