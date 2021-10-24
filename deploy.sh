f02fc81da98986387a7249444ff3776a7c3f6a03
kemvoueric <75843522+kemvoueric@users.noreply.github.com>
2021-10-24 08:10:43 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 A=$(ls -l  /var/lib/jenkins/jobs/maven-project/builds | tail -n3 | head -n1 | awk '{print $NF}')
 unset BUILD_NUMBER || true
 export BUILD_NUMBER=$A
-
+##########
 docker-compose -f /var/lib/jenkins/workspace/maven-project/docker-compose.yml up -d
