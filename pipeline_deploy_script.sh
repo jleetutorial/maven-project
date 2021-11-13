6515adeefc9cd87fde3430c428cbb4322b9e9b6e
kemvoueric <75843522+kemvoueric@users.noreply.github.com>
2021-11-13 16:22:41 -0600

    Update pipeline_deploy_script.sh

--- pipeline_deploy_script.sh
+++ pipeline_deploy_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
  unset BUILD_NUMBER || true 
- A=$(ls -l  /var/lib/jenkins/jobs/PIPELINE01/builds/ |tail -n3 | head -n1 | awk '{print $NF}')
+ A=$(ls /var/lib/jenkins/jobs/PIPELINE01/builds > file && sort -n file  | tail -1)
   export BUILD_NUMBER=$A
 
 docker-compose -f pipeline_docker-compose.yml up -d 
