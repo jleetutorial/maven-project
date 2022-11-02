e48195c1be0a30361dec33e5446541da29ca49ce
ramuboddu <113240450+ramuboddu@users.noreply.github.com>
2022-11-02 11:54:02 +0530

    everyone

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,9 @@ public class Greeter {
   public Greeter() {
 
   }
-
+ 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello everyone, %s!", someone);
   }
 }
